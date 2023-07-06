619129fb7926b894c5f0202ed64aa147281608c2
essey1 <esseyt3@gmail.com>
2023-07-06 23:31:19 +0300

    minor style changes to the scroll bar

--- library/script.js
+++ library/script.js
@@ -1,9 +1,38 @@
-let myLibrary = [];
+let myLibrary = [
+    {
+        title: "asdf",
+        author: "234",
+        page: 324
+    }
+];
 
 function book() {
 
 }
 
 function addBookToLibrary() {
-    
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const page = document.getElementById('page').value;
+    const newBook = new book(title, author, page);
+
+    myLibrary.push(newBook);
+
+    console.log(myLibrary);
 }
+
+document.getElementById('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    addBookToLibrary();
+
+    console.log(myLibrary);
+
+    
+    
+});
+
+
+
+
+
